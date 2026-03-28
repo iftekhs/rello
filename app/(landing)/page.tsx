e31eecd871e247b41b75251a6df634dbb2b562c4
iftekhs <iftekhstudio@gmail.com>
2026-03-28 19:01:53 +0600

    First commit

---
+++ app/(landing)/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Page() {
+  return (
+    <section className="h-full overflow-hidden lg:h-screen w-full">
+      hero section
+    </section>
+  );
+}
